ade09ead1039597fc3ef9eca750ca2cca1f19067
clrsahlim <clarissaliem17.2@gmail.com>
2025-06-10 12:34:28 +0700

    logout untuk admin

--- src/main/java/com/example/portaluniv/demo/controller/AuthController.java
+++ src/main/java/com/example/portaluniv/demo/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.portaluniv.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,7 +21,8 @@ import com.example.portaluniv.demo.service.DosenService;
 import com.example.portaluniv.demo.service.MahasiswaService;
 import com.example.portaluniv.demo.service.UserService;
 
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @Controller
@@ -198,4 +202,12 @@ public class AuthController {
         }
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/"; 
+    }
 }
